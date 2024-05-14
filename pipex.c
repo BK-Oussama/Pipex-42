bc4aa711247d10eefb241134647b09550ff2f785
Oussama Boukoutaya <oussama.boukoutaya@gmail.com>
2024-05-14 11:19:30 -0400

    leaks fixed

--- pipex.c
+++ pipex.c
@@ -3,21 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ouboukou <ouboukou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: boukoutaya <boukoutaya@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 18:21:50 by ouboukou          #+#    #+#             */
-/*   Updated: 2024/05/13 23:38:19 by ouboukou         ###   ########.fr       */
+/*   Updated: 2024/05/13 22:21:55 by boukoutaya       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+void	ft_free(char **s)
+{
+	int	i;
 
+	i = 0;
+	while ((s[i]))
+	{
+		free(s[i]);
+		i++;
+	}
+	free(s);	
+}
 static void	check_cmd_access(char *paths[], char *argv)
 {
 	int		i;
 	char	*c;
 	char	**cmd_args;
-
+	char	*temp;
 	cmd_args = ft_split(argv, ' ');
 	if (!cmd_args)
 		ft_putstr_fd("Pipex: ft_split failed", 2);
@@ -25,11 +36,17 @@ static void	check_cmd_access(char *paths[], char *argv)
 	while (paths[i])
 	{
 		c = ft_strjoin(paths[i], "/");
+		temp = c;
 		c = ft_strjoin(c, cmd_args[0]);
 		if (access(c, F_OK | X_OK) == 0)
 			execve(c, cmd_args, NULL);
+
+		free(c);
+		free(temp);
 		i++;
 	}
+	ft_free(cmd_args);
+	ft_free(paths);
 	(perror(""), exit(127));
 }
 
@@ -60,20 +77,20 @@ static void	retrive_paths(char **env, char *argv)
 static void	first_child(char **argv, char **env, int *fd)
 {
 	int		infile;
-	char	**cmd_op;
+	char	**cmd_args;
 
 	infile = open(argv[1], O_RDONLY, 0644);
 	if (infile < 0)
-		(perror(""), exit(EXIT_FAILURE));
+		(perror("Error opening input file"), exit(EXIT_FAILURE));
 	if (dup2(infile, STDIN_FILENO) == -1 || dup2(fd[1], STDOUT_FILENO) == -1)
-		(perror(""), exit(EXIT_FAILURE));
+		(perror("Error duplicating file descriptors"), exit(EXIT_FAILURE));
 	close(infile);
 	close(fd[0]);
 	close(fd[1]);
 	if (ft_strchr(argv[2], '/') != NULL && access(argv[2], F_OK | X_OK) == 0)
 	{
-		cmd_op = ft_split(argv[2], ' ');
-		execve(cmd_op[0], cmd_op, NULL);
+		cmd_args = ft_split(argv[2], ' ');
+		execve(cmd_args[0], cmd_args, NULL);
 	}
 	retrive_paths(env, argv[2]);
 }
@@ -81,20 +98,20 @@ static void	first_child(char **argv, char **env, int *fd)
 static void	second_child(char **argv, char **env, int *fd)
 {
 	int		outfile;
-	char	**cmd_op;
+	char	**cmd_args;
 
 	outfile = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (outfile < 0)
-		print_err_exit("", EXIT_FAILURE);
+		(perror("Error opening output file"), exit(EXIT_FAILURE));
 	if (dup2(fd[0], STDIN_FILENO) == -1 || dup2(outfile, STDOUT_FILENO) == -1)
-		(perror(""), exit(EXIT_FAILURE));
+		(perror("Error duplicating file descriptors"), exit(EXIT_FAILURE));
 	close(outfile);
 	close(fd[0]);
 	close(fd[1]);
-	if (ft_strchr(argv[2], '/') != NULL && access(argv[2], F_OK | X_OK) == 0)
+	if (ft_strchr(argv[3], '/') != NULL && access(argv[2], F_OK | X_OK) == 0)
 	{
-		cmd_op = ft_split(argv[3], ' ');
-		execve(cmd_op[0], cmd_op, NULL);
+		cmd_args = ft_split(argv[3], ' ');
+		execve(cmd_args[0], cmd_args, NULL);
 	}
 	retrive_paths(env, argv[3]);
 }
@@ -106,22 +123,24 @@ int	main(int argc, char *argv[], char *env[])
 	pid_t	child_02;
 
 	if (argc != 5)
-		ft_putstr_fd("Pipex: Please enter a valid Commands or files", 2);
+	{
+		ft_putstr_fd("Pipex: Please enter a valid Commands and files\n", 2);
+		exit(EXIT_FAILURE);
+	}
 	if (pipe(pipe_fd) == -1)
-		print_err_exit("", EXIT_FAILURE);
+		(perror("Error creating a Pipe"), exit(EXIT_FAILURE));
 	child_01 = fork();
 	if (child_01 < 0)
-		(perror(""), exit(EXIT_FAILURE));
+		(perror("Error forking first child"), exit(EXIT_FAILURE));
 	else if (child_01 == 0)
 		first_child(argv, env, pipe_fd);
 	child_02 = fork();
 	if (child_02 < 0)
-		(perror(""), exit(EXIT_FAILURE));
+		(perror("Error forking second child"), exit(EXIT_FAILURE));
 	else if (0 == child_02)
 		second_child(argv, env, pipe_fd);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
-	waitpid(child_01, NULL, 0);
-	waitpid(child_02, NULL, 0);
+	waitpid(0, NULL, 0);
 	return (EXIT_SUCCESS);
 }
